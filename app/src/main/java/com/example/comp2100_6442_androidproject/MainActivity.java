95d2ef4cf0e9986f41b123fc6c26edd57f92c830
Xiyong Tian <u6782003@anu.edu.au>
2020-04-21 13:15:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/comp2100_6442_androidproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.comp2100_6442_androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
